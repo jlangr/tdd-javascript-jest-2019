9fe05826d182552f32c0d56bbbecb336235a1e16
Jeff Langr <Jeff@Langrsoft.com>
2019-03-26 12:19:01 -0600

    name normalizer solution

--- src/misc/name-normalizer.js
+++ src/misc/name-normalizer.js
@@ -1,3 +1,24 @@
 //const countOfChar = (s, c) => (s.match(new RegExp(c, 'g'))||[]).length;
+const throwWhenNameUndefined = name => { if (name === undefined) throw new Error() }
 
-export const normalize = () => 'howzat?'
+const last = name => parts(name)[parts(name).length - 1]
+const first = name => parts(name)[0]
+const middleParts = name => parts(name).slice(1, -1)
+
+const hasMiddleName = name => parts(name).length > 2
+
+const initial = name => name.length === 1 ? ` ${name}` : ` ${name[0]}.`
+
+const middleInitials = name => {
+  if (!hasMiddleName(name)) return ''
+  return middleParts(name).map(initial).join('') }
+
+const isMononym = name => parts(name).length === 1
+
+const parts = name => name.trim().split(' ')
+
+export const normalize = name => {
+  throwWhenNameUndefined(name)
+  if (isMononym(name)) return name
+  return `${last(name)}, ${first(name)}${middleInitials(name)}`
+}
