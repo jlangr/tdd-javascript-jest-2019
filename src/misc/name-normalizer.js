efb05fe094de64faefbc06a4e7a1784ded1c41b3
Jeff Langr <Jeff@Langrsoft.com>
2019-03-28 20:11:23 -0600

    new baseline for name normalizer

--- src/misc/name-normalizer.js
+++ src/misc/name-normalizer.js
@@ -1,3 +1,17 @@
-//const countOfChar = (s, c) => (s.match(new RegExp(c, 'g'))||[]).length;
+// this will likeley prove useful and you won't
+// have to find it on StackOverflow:
+//const numberOfCharactersInString = (s, char) =>
+//  (s.match(new RegExp(char, 'g'))||[]).length;
 
-export const normalize = () => 'howzat?'
+const parts = name => name.split(' ')
+
+const last = name => parts(name)[1]
+
+const first = name => parts(name)[0]
+
+const isMononym = name => parts(name).length === 1
+
+export const normalize = name => {
+  if (isMononym(name)) return name
+  return `${last(name)}, ${first(name)}`
+}
--- src/misc/name-normalizer.test.js
+++ src/misc/name-normalizer.test.js
@@ -1,15 +1,15 @@
 import { normalize } from './name-normalizer'
 
 describe('a name normalizer', () => {
-  xit('returns empty when passed empty string', () => {
+  it('returns empty when passed empty string', () => {
     expect(normalize('')).toEqual('')
   })
 
-  xit('returns single word name', () => {
+  it('returns single word name', () => {
     expect(normalize('Plato')).toEqual('Plato')
   })
 
-  xit('swaps first and last names', () => {
+  it('swaps first and last names', () => {
     expect(normalize('Haruki Murakami')).toEqual('Murakami, Haruki')
   })
 
@@ -28,13 +28,20 @@ describe('a name normalizer', () => {
   xit('initializes each of multiple middle names', () => {
     expect(normalize('Julia Scarlett Elizabeth Louis-Dreyfus')).toEqual('Louis-Dreyfus, Julia S. E.')
   })
+
+  xit('appends suffixes to end', () => {
+    expect(normalize('Martin Luther King, Jr.')).toEqual('King, Martin L., Jr.')
+  })
+
+  xit('throws when name contains two commas', () => {
+    expect(() => { normalize('Thurston, Howell, III'); }).toThrow()
+  })
 })
 
 // Extra Credit:
-// xit('appends suffixes to end', () => {
-//   expect(normalize('Martin Luther King, Jr.')).toEqual('King, Martin L., Jr.')
-// })
-//
-// xit('throws when name contains two commas', () => {
-//   expect(() => { normalize('Thurston, Howell, III'); }).toThrow()
-// })
+//  salutations, e.g. Mr. Edmund Langr. U.S. Salutations are recognized as
+//     one of Mr., Mrs., Ms., and Dr.--either with or without the period.
+//     Anything else should be recognized as a first name
+
+// What other tests should you write? Not new features but things you know as a programmer
+// that you probably needed? (Or do you need them? Discuss.)
