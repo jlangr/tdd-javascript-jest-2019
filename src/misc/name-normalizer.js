e7c736878b5dcdb661695aca82ad7d0c35c1c12e
Jeff Langr <Jeff@Langrsoft.com>
2019-08-13 11:08:34 -0500

    initial exercises solutions

--- src/misc/name-normalizer.js
+++ src/misc/name-normalizer.js
@@ -1,17 +1,45 @@
-// this will likeley prove useful and you won't
-// have to find it on StackOverflow:
-//const numberOfCharactersInString = (s, char) =>
-//  (s.match(new RegExp(char, 'g'))||[]).length;
+const numberOfCharactersInString = (s, char) =>
+  (s.match(new RegExp(char, 'g'))||[]).length;
 
-const parts = name => name.split(' ')
+const parts = name => name.trim().split(' ')
 
-const last = name => parts(name)[1]
+const last = name => parts(name)[parts(name).length - 1]
 
 const first = name => parts(name)[0]
 
+const middleNames = name => parts(name).slice(1, -1)
+
+const middleInitial = midName =>
+  midName.length > 1 ? midName[0] + '.' : midName[0]
+
+const middleInitials = name => middleNames(name).map(middleInitial).join(' ')
+
 const isMononym = name => parts(name).length === 1
 
+const hasMiddleName = name => parts(name).length > 2
+
+const hasSuffix = name => name.split(',').length > 1
+
+const suffix = name => hasSuffix(name) ? `,${name.split(',')[1]}` : ""
+
+let extractBaseNameAndSuffix = function (name) {
+  return name.split(',')
+}
+
+const throwOnTooManyCommas = (name) => {
+  if (numberOfCharactersInString(name,',') > 1)
+    throw new Error()
+}
+
 export const normalize = name => {
+  throwOnTooManyCommas(name)
+
   if (isMononym(name)) return name
-  return `${last(name)}, ${first(name)}`
+
+  const [fullName] = extractBaseNameAndSuffix(name)
+
+  if (hasMiddleName(fullName)) {
+    return `${last(fullName)}, ${first(fullName)} ${middleInitials(fullName)}${suffix(name)}`
+  }
+  return `${last(fullName)}, ${first(fullName)}${suffix(name)}`
 }
