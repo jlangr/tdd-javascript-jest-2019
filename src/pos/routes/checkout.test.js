9ccdbf3cfab3af916718a1665dcf7ad9e2a911c3
Jeff Langr <Jeff@Langrsoft.com>
2019-09-13 17:43:24 -0600

    almost done reworking tests

--- src/pos/routes/checkout.test.js
+++ src/pos/routes/checkout.test.js
@@ -122,285 +122,148 @@ describe('checkout routes', () => {
     })
   })
 
-  // In Progress
   describe('checkout total', () => {
-    // it('does stuff', () => {
-    //   IncrementingIdGenerator.reset(checkoutId)
-    //   postCheckout({}, response)
-    //   sendSpy.resetHistory()
-    //   // set up for discountng
-    //   itemDatabaseRetrieveStub.callsFake(_ => ({ upc: '333', price: 3.33, description: '', exempt: false }))
-    //   postItem({ params: { id: checkoutId }, body: { upc: '333' } }, response)
-    //   sendSpy.resetHistory()
-    //   console.log('req id', checkoutId )
-    //   itemDatabaseRetrieveStub.callsFake(_ => ({ upc: '444', price: 4.44, description: '', exempt: false }))
-    //   postItem({ params: { id: checkoutId }, body: { upc: '444' } }, response)
-    //   sendSpy.resetHistory()
-    //   const request = { params: { id: checkoutId }}
-    //   postCheckoutTotal(request, response)
-    //   expect(response.status).toEqual(200)
-    //   console.log('reseponse status', response.status)
-    //   expect(sinon.assert.calledWith(response.send, sinon.match({ total: 7.77 })))
-    //
-    //   //  not found
-    //   postCheckoutTotal({ params: { id: 'unknown' }}, response)
-    //   expect(response.status).toEqual(400)
-    //   sinon.assert.calledWith(response.send, { error: 'nonexistent checkout' })
-    // })
-    //
-    // it('applies any member discount', () => {
-    //   IncrementingIdGenerator.reset(checkoutId)
-    //   postCheckout({}, response)
-    //   sendSpy.resetHistory()
-    //   scanMember('719-287-4335', 0.25)
-    //   purchase('333', 3.33)
-    //   purchase('444', 4.44)
-    //   postCheckoutTotal({ params: { id: checkoutId }}, response)
-    //   expect(sinon.assert.calledWith(response.send, sinon.match({ total: 5.83 })))
-    // })
-    //
-    // it('3rd disc test', () => {
-    //   IncrementingIdGenerator.reset(1001)
-    //   postCheckout({}, response)
-    //   sendSpy.resetHistory()
-    //   scanMember('719-287-4335', 0.085)
-    //   purchase('333', 4.40)
-    //   purchaseExemptItem('444', 5.50)
-    //   postCheckoutTotal({ params: { id: checkoutId }}, response)
-    //   expect(sinon.assert.calledWith(response.send, sinon.match({ total: 9.53 })))
-    // })
-    //
-    // it('discd tots', () => {
-    //   IncrementingIdGenerator.reset(1001)
-    //   postCheckout({}, response)
-    //   sendSpy.resetHistory()
-    //   purchaseExemptItem('444', 6.00)
-    //   scanMember('719-287-4335', 0.10)
-    //   purchase('333', 4.00)
-    //   postCheckoutTotal({ params: { id: checkoutId }}, response)
-    //   expect(sinon.assert.calledWith(response.send, sinon.match({ totalOfDiscountedItems:  3.60 })))
-    //   sendSpy.resetHistory()
-    //
-    //   // amount saved
-    //   IncrementingIdGenerator.reset(1001)
-    //   postCheckout({}, response)
-    //   sendSpy.resetHistory()
-    //   scanMember('719-287-4335', 0.10)
-    //   purchase('333', 4.00)
-    //   purchase('444', 6.00)
-    //   postCheckoutTotal({ params: { id: checkoutId }}, response)
-    //   expect(sinon.assert.calledWith(response.send, sinon.match({ /* totalOfDiscountedItems*/totalSaved:  1.00 })))
-    // })
-    //
-    // it('provides 0 total for discounted items when no member scanned', () => {
-    //   IncrementingIdGenerator.reset(1001)
-    //   postCheckout({}, response)
-    //   sendSpy.resetHistory()
-    //   purchase('333', 4.00)
-    //   postCheckoutTotal({ params: { id: checkoutId }}, response)
-    //   expect(sinon.assert.calledWith(response.send, sinon.match({ totalOfDiscountedItems :   0.00 })))
-    // })
-    //
-    // it('provides 0 total for discounted items when member discount is 0', () => {
-    //   IncrementingIdGenerator.reset(1001)
-    //   postCheckout({}, response)
-    //   sendSpy.resetHistory()
-    //   scanMember('719-287-4335', 0.00)
-    //   // addCheckout(checkoutId)
-    //
-    //   purchase('333', 4.00)
-    //
-    //   postCheckoutTotal({ params: { id: checkoutId }}, response)
-    //   expect(sinon.assert.calledWith(response.send, sinon.match({ totalOfDiscountedItems :   0.00 })))
-    // })
-  })
+    beforeEach(() => {
+      response = createEmptyResponse()
+    })
 
-})
+    const purchaseItem = (upc, price, description, exempt = false) => {
+      overrideRetrieveItem(upc => ({ upc, price, description, exempt }))
+      const response = createEmptyResponse()
+      postItem({ params: { id: checkoutId }, body: { upc } }, response)
+    }
 
-/*
+    const purchaseExemptItem = (upc, price, description='') => {
+      purchaseItem(upc, price, description, true)
+    }
 
+    const purchase = (upc, price, description='') => {
+      purchaseItem(upc, price, description, false)
+    }
 
- */
+    const scanMember = (id, discount, name = 'Jeff Languid') => {
+      overrideRetrieveMember(() => ({ member: id, discount, name }))
+      const response = createEmptyResponse()
+      postMember({ params: { id: checkoutId }, body: { id }}, response)
+    }
 
-xdescribe('checkout functionality', () => {
-  let response
-  let sendSpy
-  let itemDatabaseRetrieveStub
-  let memberDatabaseRetrieveStub
-
-  const checkoutId = 1001
-
-  beforeEach(() => {
-    sendSpy = sinon.spy()
-    sendSpy.resetHistory()
-    response = emptyResponse()
-    clearAllCheckouts()
-  })
-
-  beforeEach(() => itemDatabaseRetrieveStub = sinon.stub(ItemDatabase.prototype, 'retrieve'))
-  beforeEach(() => memberDatabaseRetrieveStub = sinon.stub(MemberDatabase.prototype, 'retrieve'))
-
-  afterEach(() => itemDatabaseRetrieveStub.restore())
-  afterEach(() => memberDatabaseRetrieveStub.restore())
-
-  const purchaseItem = (upc, price, description, exempt = false) => { 
-    itemDatabaseRetrieveStub.callsFake(upc => ({ upc, price, description, exempt }))
-    postItem({ params: { id: checkoutId }, body: { upc } }, response)
-    sendSpy.resetHistory()
-  }
-
-  const purchaseExemptItem = (upc, price, description='') => { 
-    purchaseItem(upc, price, description, true)
-  }
-
-  const purchase = (upc, price, description='') => { 
-    purchaseItem(upc, price, description, false)
-  }
-
-  const scanMember = (id, discount, name = 'Jeff Languid') => {
-    memberDatabaseRetrieveStub.callsFake(_ => ({ member: id, discount, name }))
-    postMember({ params: { id: checkoutId }, body: { id }}, response)
-    sendSpy.resetHistory()
-  }
-
-  const emptyResponse = () => ({ 
-    send: sendSpy,
-    status: undefined 
-  })
-
-  const expectResponseMatches = expected =>
-    sinon.assert.calledWith(response.send, sinon.match(expected))
-
-  const expectResponseEquals = expected => 
-    sinon.assert.calledWith(response.send, expected)
-
-  const addCheckout = id => {
-    IncrementingIdGenerator.reset(id)
-    postCheckout({}, response)
-    sendSpy.resetHistory()
-  }
-
-  describe('checkout total', () => {
     it('does stuff', () => {
       IncrementingIdGenerator.reset(checkoutId)
       postCheckout({}, response)
-      sendSpy.resetHistory()
+      overrideRetrieveItem(() => {})
       // set up for discountng
-      itemDatabaseRetrieveStub.callsFake(_ => ({ upc: '333', price: 3.33, description: '', exempt: false }))
+      overrideRetrieveItem(() => ({ upc: '333', price: 3.33, description: '', exempt: false }))
       postItem({ params: { id: checkoutId }, body: { upc: '333' } }, response)
-      sendSpy.resetHistory()
+      overrideRetrieveItem(() => {})
       console.log('req id', checkoutId )
-      itemDatabaseRetrieveStub.callsFake(_ => ({ upc: '444', price: 4.44, description: '', exempt: false }))
+      overrideRetrieveItem(() => ({ upc: '444', price: 4.44, description: '', exempt: false }))
       postItem({ params: { id: checkoutId }, body: { upc: '444' } }, response)
-      sendSpy.resetHistory()
+      overrideRetrieveItem(() => {})
       const request = { params: { id: checkoutId }}
+      response = createEmptyResponse()
       postCheckoutTotal(request, response)
       expect(response.status).toEqual(200)
       console.log('reseponse status', response.status)
-      expect(sinon.assert.calledWith(response.send, sinon.match({ total: 7.77 })))
+      const firstCallFirstArg = response.send.mock.calls[0][0]
+      expect(firstCallFirstArg).toMatchObject({ total: 7.77 })
 
       //  not found
       postCheckoutTotal({ params: { id: 'unknown' }}, response)
       expect(response.status).toEqual(400)
-      sinon.assert.calledWith(response.send, { error: 'nonexistent checkout' })
+      expect(response.send).toHaveBeenCalledWith({ error: 'nonexistent checkout' })
     })
 
     it('applies any member discount', () => {
-      IncrementingIdGenerator.reset(checkoutId)
-      postCheckout({}, response)
-      sendSpy.resetHistory()
       scanMember('719-287-4335', 0.25)
       purchase('333', 3.33)
       purchase('444', 4.44)
+
       postCheckoutTotal({ params: { id: checkoutId }}, response)
-      expect(sinon.assert.calledWith(response.send, sinon.match({ total: 5.83 })))
+
+      expectResponseSentToMatch(response, { total: 5.83 })
     })
 
     it('3rd disc test', () => {
-      IncrementingIdGenerator.reset(1001)
-      postCheckout({}, response)
-      sendSpy.resetHistory()
       scanMember('719-287-4335', 0.085)
       purchase('333', 4.40)
       purchaseExemptItem('444', 5.50)
+      response = createEmptyResponse()
+
       postCheckoutTotal({ params: { id: checkoutId }}, response)
-      expect(sinon.assert.calledWith(response.send, sinon.match({ total: 9.53 })))
+
+      expectResponseSentToMatch(response, { total: 9.53 })
     })
 
     it('discd tots', () => {
-      IncrementingIdGenerator.reset(1001)
-      postCheckout({}, response)
-      sendSpy.resetHistory()
       purchaseExemptItem('444', 6.00)
       scanMember('719-287-4335', 0.10)
       purchase('333', 4.00)
       postCheckoutTotal({ params: { id: checkoutId }}, response)
-      expect(sinon.assert.calledWith(response.send, sinon.match({ totalOfDiscountedItems:  3.60 })))
-      sendSpy.resetHistory()
+      expectResponseSentToMatch(response, { totalOfDiscountedItems:  3.60 })
 
       // amount saved
       IncrementingIdGenerator.reset(1001)
       postCheckout({}, response)
-      sendSpy.resetHistory()
       scanMember('719-287-4335', 0.10)
       purchase('333', 4.00)
       purchase('444', 6.00)
+      response = createEmptyResponse()
       postCheckoutTotal({ params: { id: checkoutId }}, response)
-      expect(sinon.assert.calledWith(response.send, sinon.match({ /* totalOfDiscountedItems*/totalSaved:  1.00 })))
+      expectResponseSentToMatch(response, { /* totalOfDiscountedItems*/totalSaved:  1.00 })
     })
 
     it('provides 0 total for discounted items when no member scanned', () => {
-      IncrementingIdGenerator.reset(1001)
-      postCheckout({}, response)
-      sendSpy.resetHistory()
+      postCheckout({}, createEmptyResponse())
       purchase('333', 4.00)
       postCheckoutTotal({ params: { id: checkoutId }}, response)
-      expect(sinon.assert.calledWith(response.send, sinon.match({ totalOfDiscountedItems :   0.00 })))
+      expectResponseSentToMatch(response, { totalOfDiscountedItems :   0.00 })
     })
 
     it('provides 0 total for discounted items when member discount is 0', () => {
       IncrementingIdGenerator.reset(1001)
-      postCheckout({}, response)
-      sendSpy.resetHistory()
+      postCheckout({}, createEmptyResponse())
       scanMember('719-287-4335', 0.00)
       // addCheckout(checkoutId)
 
       purchase('333', 4.00)
 
       postCheckoutTotal({ params: { id: checkoutId }}, response)
-      expect(sinon.assert.calledWith(response.send, sinon.match({ totalOfDiscountedItems :   0.00 })))
+      expectResponseSentToMatch(response, { totalOfDiscountedItems :   0.00 })
     })
   })
 
+
+  // In Progress
   describe('message lines', () => {
-    beforeEach(() => addCheckout(checkoutId))
+    // beforeEach(() => addCheckout(checkoutId))
 
-    it('includes items and total', () => {
-      purchase('123', 5.00, 'Milk')
-      purchase('555', 12.00, 'Fancy eggs')
-
-      postCheckoutTotal({ params: { id: checkoutId } }, response)
-
-      expectResponseMatches(
-        { messages: ['Milk                                     5.00',
-          'Fancy eggs                              12.00',
-          'TOTAL                                   17.00' ]})
-    })
-
-    it('includes discounts and total saved', () => {
-      scanMember('719-287-4335', 0.10)
-      purchase('123', 5.00, 'Milk')
-      purchase('555', 2.79, 'Eggs')
-
-      postCheckoutTotal({ params: { id: checkoutId } }, response)
-
-      expectResponseMatches(
-        { messages: ['Milk                                     5.00',
-          '   10% mbr disc                         -0.50',
-          'Eggs                                     2.79',
-          '   10% mbr disc                         -0.28',
-          'TOTAL                                    7.01',
-          '*** You saved:                           0.78' ] })
-    })
+    // it('includes items and total', () => {
+    //   purchase('123', 5.00, 'Milk')
+    //   purchase('555', 12.00, 'Fancy eggs')
+    //
+    //   postCheckoutTotal({ params: { id: checkoutId } }, response)
+    //
+    //   expectResponseMatches(
+    //     { messages: ['Milk                                     5.00',
+    //         'Fancy eggs                              12.00',
+    //         'TOTAL                                   17.00' ]})
+    // })
+    //
+    // it('includes discounts and total saved', () => {
+    //   scanMember('719-287-4335', 0.10)
+    //   purchase('123', 5.00, 'Milk')
+    //   purchase('555', 2.79, 'Eggs')
+    //
+    //   postCheckoutTotal({ params: { id: checkoutId } }, response)
+    //
+    //   expectResponseMatches(
+    //     { messages: ['Milk                                     5.00',
+    //         '   10% mbr disc                         -0.50',
+    //         'Eggs                                     2.79',
+    //         '   10% mbr disc                         -0.28',
+    //         'TOTAL                                    7.01',
+    //         '*** You saved:                           0.78' ] })
+    // })
   })
 })
+
