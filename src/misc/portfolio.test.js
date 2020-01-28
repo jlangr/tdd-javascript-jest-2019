5068887366ef7fa7016325f60720b7bec7b10439
Jeff Langr <Jeff@Langrsoft.com>
2020-01-28 17:17:05 -0600

    portfolio finished

--- src/misc/portfolio.test.js
+++ src/misc/portfolio.test.js
@@ -42,12 +42,63 @@ describe('a portfolio', () => {
       expect(Portfolio.symbolCount(newPortfolio)).toEqual(1)
     })
 
+
 // TODO: eliminate duplication in tests
 
     it('does not alter passed portfolio on purchase', () => {
       Portfolio.purchase(portfolio, 'BAYN', 1)
 
-      expect(portfolio.uniqueSymbols).toEqual(new Set())
+      // expect(portfolio.holdings).toEqual({})
+      expect(Portfolio.isEmpty(portfolio)).toBe(true)
+    })
+  })
+
+  describe('share count', () => {
+    it('represents shares purchased', () => {
+      const newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 50)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(50)
+    })
+
+    it('is zero when never purchased', () => {
+      expect(Portfolio.sharesOf(Portfolio.create(), 'BAYN'))
+        .toEqual(0)
+    })
+
+    it('differs by symbol', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 10)
+      newPortfolio = Portfolio.purchase(newPortfolio, 'IBM', 5)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(10)
+    })
+
+    it('accumulates on multiple purchases', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 20)
+      newPortfolio = Portfolio.purchase(newPortfolio, 'BAYN', 22)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(42)
+    })
+  })
+
+  describe('when selling', () => {
+    it('reduces share count', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 20)
+
+      newPortfolio = Portfolio.sell(newPortfolio, 'BAYN', 3)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(17)
+    })
+
+    it('results in empty portfolio when selling everything', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 5)
+
+      newPortfolio = Portfolio.sell(newPortfolio, 'BAYN', 5)
+
+      expect(Portfolio.isEmpty(newPortfolio)).toBe(true)
+    })
+
+    it('throws when selling too many', () => {
+      expect(() => Portfolio.sell(portfolio, 'BAYN', 1)).toThrow(RangeError)
     })
   })
 })
