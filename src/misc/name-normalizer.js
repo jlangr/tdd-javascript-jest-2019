14fa2bbbde0d0f9a78d3d570ad167a182e32037c
Jeff Langr <Jeff@Langrsoft.com>
2020-01-28 11:05:10 -0600

    basics and Name Normalizer

--- src/misc/name-normalizer.js
+++ src/misc/name-normalizer.js
@@ -1,17 +1,47 @@
-// this will likeley prove useful and you won't
-// have to find it on StackOverflow:
-//const numberOfCharactersInString = (s, char) =>
-//  (s.match(new RegExp(char, 'g'))||[]).length;
+const numberOfCharactersInString = (s, char) =>
+ (s.match(new RegExp(char, 'g'))||[]).length;
 
 const parts = name => name.split(' ')
 
-const last = name => parts(name)[1]
+const last = name => name.parts[name.parts.length - 1]
 
-const first = name => parts(name)[0]
+const first = name => name.parts[0]
 
-const isMononym = name => parts(name).length === 1
+const isMononym = name => name.parts.length === 1
 
-export const normalize = name => {
-  if (isMononym(name)) return name
-  return `${last(name)}, ${first(name)}`
+const initial = name => name.length === 1 ? ` ${name}` : ` ${name[0]}.`
+
+const middleParts = name => name.parts.slice(1, -1)
+
+const middleInitials = name => middleParts(name).map(initial).join('')
+
+const suffixless = name => {
+  const [baseName] = name.split(',')
+  return baseName
+}
+
+const suffix = name => {
+  const [_, afterComma] = name.fullName.split(',')
+  return afterComma ? `,${afterComma}` : ''
+}
+
+const throwOnExcessCommas = name => {
+  if (numberOfCharactersInString(name, ',') > 1) throw new Error()
+}
+
+const parse = fullName => {
+  throwOnExcessCommas(fullName)
+
+  const trimmedName = fullName.trim()
+  const baseName = suffixless(trimmedName)
+  return { baseName, trimmedName, fullName, parts: parts(baseName) }
+}
+
+const formatMononym = name => name.trimmedName
+const formatWestern = name => `${last(name)}, ${first(name)}${middleInitials(name)}${suffix(name)}`
+
+export const normalize = fullName => {
+  const name = parse(fullName)
+  if (isMononym(name)) return formatMononym(name)
+  return formatWestern(name)
 }
