1ed299c7625b3a3c398c6567eeeea6939645ba78
Jeff Langr <Jeff@Langrsoft.com>
2020-02-11 16:40:08 -0600

    end portfolio solution

--- src/misc/portfolio.js
+++ src/misc/portfolio.js
@@ -1,15 +1,39 @@
-export const uniqueSymbolCount = portfolio =>
-  portfolio.symbols.size
-
-export const purchase = (portfolio, symbol, _shares) => {
-  let symbolWithNewSymbolAdded = new Set(portfolio.symbols)
-  symbolWithNewSymbolAdded.add(symbol)
-  return ({
-    ...portfolio,
-    symbols: symbolWithNewSymbolAdded
-  })
-}
-
-export const create = () => ({ symbols: new Set() })
+export const create = () => ({ holdings: {} })
 
 export const isEmpty = portfolio => uniqueSymbolCount(portfolio) === 0
+
+export const uniqueSymbolCount = portfolio =>
+  Object.keys(portfolio.holdings).length
+
+export const sharesOf = (portfolio, symbol) =>
+  symbol in portfolio.holdings
+    ? portfolio.holdings[symbol]
+    : 0
+
+export const purchase = (portfolio, symbol, shares) =>
+  transact(portfolio, symbol, shares)
+
+export const sell = (portfolio, symbol, shares) => {
+  throwWhenSellingTooMany(portfolio, symbol, shares)
+  const newPortfolio = transact(portfolio, symbol, -shares)
+  return removeSymbolIfAllSold(newPortfolio, symbol)
+}
+
+export const transact = (portfolio, symbol, shares) =>
+  ({ ...portfolio,
+    holdings: {
+      ...portfolio.holdings, [symbol]: sharesOf(portfolio, symbol) + shares
+    }
+  })
+
+const throwWhenSellingTooMany = (portfolio, symbol, shares) => {
+  if (shares > sharesOf(portfolio, symbol)) throw new RangeError()
+}
+
+const removeSymbolIfAllSold = (portfolio, soldSymbol) => {
+  if (sharesOf(portfolio, soldSymbol) > 0) return portfolio
+
+  let newHoldings = { ...portfolio.holdings }
+  delete newHoldings[soldSymbol]
+  return { ...portfolio, holdings: newHoldings }
+}
--- src/misc/portfolio.test.js
+++ src/misc/portfolio.test.js
@@ -24,13 +24,13 @@ describe('a portfolio', () => {
       expect(Portfolio.uniqueSymbolCount(portfolio)).toBe(0)
     })
 
-    it('is 1 when a purchase is made', () => {
+    it('is 1 when a single purchase is made', () => {
       const newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 1)
 
       expect(Portfolio.uniqueSymbolCount(newPortfolio)).toBe(1)
     })
 
-    it('is incremented on multiple purchases different symbol', () => {
+    it('is incremented with each new symbol purchase', () => {
       let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 1)
       newPortfolio = Portfolio.purchase(newPortfolio, 'IBM', 1)
 
@@ -45,4 +45,58 @@ describe('a portfolio', () => {
     })
   })
 
+  describe('shares by symbol', () => {
+    it('is returned for single purchase', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 42)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(42)
+    })
+
+    it('is 0 for symbol not bought', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'IBM', 42)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(0)
+    })
+
+    it('accumulates for multiple purchases same symbol', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'IBM', 42)
+      newPortfolio = Portfolio.purchase(newPortfolio, 'IBM', 10)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'IBM')).toEqual(52)
+    })
+
+    it('returns appropriate shares for given symbol', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'IBM', 42)
+      newPortfolio = Portfolio.purchase(newPortfolio, 'BAYN', 10)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'IBM')).toEqual(42)
+    })
+  })
+
+  describe('selling', () => {
+    it('decreases shares by shares sold', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 42)
+      newPortfolio = Portfolio.sell(newPortfolio, 'BAYN', 10)
+
+      expect(Portfolio.sharesOf(newPortfolio, 'BAYN')).toEqual(32)
+    })
+
+    it('decrements symbol count when all shares sold', () => {
+      let newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 42)
+      newPortfolio = Portfolio.purchase(newPortfolio, 'IBM', 22)
+      newPortfolio = Portfolio.sell(newPortfolio, 'IBM', 22)
+
+      expect(Portfolio.uniqueSymbolCount(newPortfolio)).toEqual(1)
+    })
+  })
+
+  describe('potential errors', () => {
+    it('throws when selling too many', () => {
+      const newPortfolio = Portfolio.purchase(portfolio, 'BAYN', 20)
+
+      expect(() => {
+        Portfolio.sell(newPortfolio, 'IBM', 20 + 1)
+      }).toThrow(RangeError)
+    })
+  })
 })
